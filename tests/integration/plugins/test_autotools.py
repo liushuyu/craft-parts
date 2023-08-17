f5b85acefe8e74bacc1423ed8d077cd91a5d609d
liushuyu <liushuyu011@gmail.com>
2023-08-17 13:10:38 -0600

    tests: fix pyright errors

--- tests/integration/plugins/test_autotools.py
+++ tests/integration/plugins/test_autotools.py
@@ -1,1 +1,2 @@
+"""Tests for the `autotools` plugin."""
 # TODO: add autotools plugin integration test after executor lands
--- tests/integration/plugins/test_make.py
+++ tests/integration/plugins/test_make.py
@@ -1,1 +1,2 @@
+"""Tests for the `make` plugin."""
 # TODO: add make plugin integration test after executor lands
